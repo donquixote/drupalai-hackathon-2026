78974b8fa81516374ac31259addc289de6cd419d
Baris Tosun <baris@acolono.com>
2026-01-28 11:42:37 +0100

    Work on the image saving

--- web/modules/team8/src/Hook/CanvasPagePreviewImageUpdate.php
+++ web/modules/team8/src/Hook/CanvasPagePreviewImageUpdate.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Drupal\team8\Hook;
 
 use Drupal\canvas\Entity\Page;
+use Drupal\Core\File\FileSystemInterface;
 use Drupal\Core\Hook\Attribute\Hook;
 use Drupal\file\Entity\File;
+use Gotenberg\Gotenberg;
 
 class CanvasPagePreviewImageUpdate {
 
@@ -31,7 +33,59 @@ class CanvasPagePreviewImageUpdate {
     if (!$image) {
       return;
     }
+
+
+
+
+
+    // Example HTML source – replace with your real canvas HTML.
+    $url = 'https://www.google.com';
+
+    // Call Gotenberg and generate an image.
+    $response = Gotenberg::chromium('http://gotenberg:3000')
+      ->screenshot()
+      ->url($url);
+
+    $binary = $response->getBody()->getContents();
+
+    if (empty($binary)) {
+      return;
+    }
+
+    $file_system = \Drupal::service('file_system');
+    $file_repository = \Drupal::service('file.repository');
+
+    $directory = 'public://canvas_page_previews';
+    $file_system->prepareDirectory(
+      $directory,
+      FileSystemInterface::CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS
+    );
+
+    $filename = sprintf(
+      'canvas_page_preview_%d_%d.png',
+      $page->id(),
+      \Drupal::time()->getRequestTime()
+    );
+
+    $destination = $directory . '/' . $filename;
+
+    // Write image returned by Gotenberg.
+    $image = $file_repository->writeData(
+      $binary,
+      $destination,
+      FileSystemInterface::EXISTS_REPLACE
+    );
+
+    $image->setPermanent();
     $image->save();
+
+
+
+
+
+
+
+//    $image->save();
   }
 
 }
